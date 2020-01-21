035c11320a1a635784a8f3dd79a7b8e034847b8a
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-01-21 14:21:07 +0100

    [11.0][FIX] medical_exam year instead of date

--- cb_hr_views/models/hr_employee_medical_examination.py
+++ cb_hr_views/models/hr_employee_medical_examination.py
@@ -1,6 +1,8 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from datetime import date
+
 from odoo import api, fields, models
 
 
@@ -9,6 +11,8 @@ class HrEmployeeMedicalExamination(models.Model):
     _inherit = "hr.employee.medical.examination"
 
     state = fields.Selection(selection_add=[("rejected", "Rejected")])
+
+    year = fields.Integer("Year", default=date.today().year)
 
     @api.multi
     def back_to_pending(self):
