4275f90365ed9d0a623784cede502dcb96e562f3
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-13 14:17:41 +0100

    [11.0][IMP] add colors based on states to holidays

--- cb_hr_views/models/hr_holidays.py
+++ cb_hr_views/models/hr_holidays.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class HrHolidays(models.Model):
@@ -15,3 +15,17 @@ class HrHolidays(models.Model):
         if not result.get("partner_ids", False):
             result["partner_ids"] = [(4, self.employee_id.partner_id.id)]
         return result
+
+    tree_color = fields.Char(compute="_compute_color", store=True)
+
+    @api.depends("state")
+    def _compute_color(self):
+        for record in self:
+            if record.state == "validate":
+                record.tree_color = "#e2ffe6"
+            elif record.state == "validate1":
+                record.tree_color = "#e2f0ff"
+            elif record.state == "refuse":
+                record.tree_color = "#ffefef"
+            else:
+                record.tree_color = "#ffffff"
