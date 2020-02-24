10eded29d3c1751687d863498890df61a6ee3374
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-24 17:26:08 +0100

    [12.0][MIG] cb_holidays_minimum_days

--- cb_holidays_minimum_days/__manifest__.py
+++ cb_holidays_minimum_days/__manifest__.py
@@ -3,12 +3,11 @@
 
 {
     "name": "Cb Holidays Minimum Days",
-    "description": """
-        Minimum leaves for holidays""",
-    "version": "11.0.1.0.0",
+    "summary": """Minimum leaves for holidays""",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca",
     "website": "www.creublanca.es",
     "depends": ["hr_holidays"],
-    "data": ["views/hr_holidays_views.xml"],
+    "data": ["views/hr_leave_views.xml"],
 }
