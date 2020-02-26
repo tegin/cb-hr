cc820dc60ec028e87821762b5df081bcd513773a
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-02-26 13:17:20 +0100

    [12.0][MIG] cb_hr_views

--- cb_hr_views/models/resource_calendar.py
+++ cb_hr_views/models/resource_calendar.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.addons.resource.models.resource import float_to_time
 
 
 class ResourceCalendar(models.Model):
@@ -15,3 +16,28 @@ class ResourceCalendar(models.Model):
     def toggle_archive_calendar(self):
         for record in self:
             record.not_archived = not record.not_archived
+
+    @api.multi
+    def _get_day_attendances(self, day_date, start_time, end_time):
+        """ Given a day date, return matching attendances. Those can be limited
+        by starting and ending time objects. """
+        self.ensure_one()
+        weekday = day_date.weekday()
+        attendances = self.env["resource.calendar.attendance"]
+
+        for attendance in self.attendance_ids.filtered(
+            lambda att: int(att.dayofweek) == weekday
+            and not (
+                att.date_from
+                and fields.Date.from_string(att.date_from) > day_date
+            )
+            and not (
+                att.date_to and fields.Date.from_string(att.date_to) < day_date
+            )
+        ):
+            if start_time and float_to_time(attendance.hour_to) < start_time:
+                continue
+            if end_time and float_to_time(attendance.hour_from) > end_time:
+                continue
+            attendances |= attendance
+        return attendances
