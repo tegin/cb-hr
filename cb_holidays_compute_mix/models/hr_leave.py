1c806bce2b7447744a1b299989a2a4eba7d09461
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-04-07 10:57:49 +0200

    [12.0][FIX] approval update approve your own

--- cb_holidays_compute_mix/models/hr_leave.py
+++ cb_holidays_compute_mix/models/hr_leave.py
@@ -5,7 +5,8 @@
 import math
 from datetime import datetime
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 from odoo.addons.resource.models.resource import float_to_time
 from odoo.addons.hr_holidays.models.hr_leave import DummyAttendance
 
@@ -192,6 +193,71 @@ class HrLeave(models.Model):
             lambda request: request.holiday_type == "employee"
         )
         holidays._create_resource_leave()
+
+    def _check_approval_update(self, state):
+        """ Check if target state is achievable. """
+        current_employee = self.env["hr.employee"].search(
+            [("user_id", "=", self.env.uid)], limit=1
+        )
+        is_officer = self.env.user.has_group(
+            "hr_holidays.group_hr_holidays_user"
+        )
+        is_manager = self.env.user.has_group(
+            "hr_holidays.group_hr_holidays_manager"
+        )
+        for holiday in self:
+            val_type = holiday.holiday_status_id.validation_type
+            if state == "confirm":
+                continue
+
+            if state == "draft":
+                if holiday.employee_id != current_employee and not is_manager:
+                    raise UserError(
+                        _(
+                            "Only a Leave Manager can reset other people leaves."
+                        )
+                    )
+                continue
+
+            if not is_officer:
+                raise UserError(
+                    _(
+                        "Only a Leave Officer or Manager can approve or refuse leave requests."
+                    )
+                )
+
+            if is_officer:
+                # use ir.rule based first access check: department, members, ... (see security.xml)
+                holiday.check_access_rule("write")
+
+            if (state == "validate1" and val_type == "both") or (
+                state == "validate" and val_type == "manager"
+            ):
+                manager = (
+                    holiday.employee_id.parent_id
+                    or holiday.employee_id.department_id.manager_id
+                )
+                if (
+                    manager and manager != current_employee
+                ) and not self.env.user.has_group(
+                    "hr_holidays.group_hr_holidays_manager"
+                ):
+                    raise UserError(
+                        _(
+                            "You must be either %s's manager or Leave manager to approve this leave"
+                        )
+                        % (holiday.employee_id.name)
+                    )
+
+            if state == "validate" and val_type == "both":
+                if not self.env.user.has_group(
+                    "hr_holidays.group_hr_holidays_manager"
+                ):
+                    raise UserError(
+                        _(
+                            "Only an Leave Manager can apply the second approval on leave requests."
+                        )
+                    )
 
 
 class HolidaysAllocation(models.Model):
