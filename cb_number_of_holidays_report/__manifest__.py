8ea77325059b7dc86014f12f35b3aa45eede11a3
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-09-23 12:56:32 +0200

    [12.0][IMP] cb_number_of_holidays_report: specify types included in report

--- cb_number_of_holidays_report/__manifest__.py
+++ cb_number_of_holidays_report/__manifest__.py
@@ -2,19 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Cb Number Of Holidays Report',
-    'summary': """
+    "name": "Cb Number Of Holidays Report",
+    "summary": """
         Report para saber quien tiene vacaciones en un intervalo de tiempo""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca',
-    'website': 'www.creublanca.es',
-    'depends': [
-        'cb_holidays_compute_mix',
-        'cb_hr_views',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca",
+    "website": "www.creublanca.es",
+    "depends": ["cb_holidays_compute_mix", "cb_hr_views"],
+    "data": [
+        "views/hr_leave_type.xml",
+        "report/holidays_count_report.xml",
+        "wizards/wizard_holidays_count.xml",
     ],
-    'data': [
-        'report/holidays_count_report.xml',
-        'wizards/wizard_holidays_count.xml',
-    ],
+    "pre_init_hook": "pre_init_hook",
 }
