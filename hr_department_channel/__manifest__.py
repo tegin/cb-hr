b92fd6cb3e9bc4f1354d55ff25c86f61f874ceac
Jaime Arroyo <jaime.arroyo@creublanca.es>
2020-11-05 08:28:09 +0100

    [FIX] Pre-commit

--- hr_department_channel/__manifest__.py
+++ hr_department_channel/__manifest__.py
@@ -2,19 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hr Department Channel',
-    'summary': """
+    "name": "Hr Department Channel",
+    "summary": """
         Create Channels from department""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'Creu Blanca,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr',
-        'mail',
-    ],
-    'data': [
-        'wizards/wizard_create_department_channel.xml',
-        'views/hr_department.xml',
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Creu Blanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/hr",
+    "depends": ["hr", "mail"],
+    "data": [
+        "wizards/wizard_create_department_channel.xml",
+        "views/hr_department.xml",
     ],
 }
--- hr_department_channel/models/hr_department.py
+++ hr_department_channel/models/hr_department.py
@@ -5,19 +5,22 @@ from odoo import api, fields, models
 
 
 class HrDepartment(models.Model):
-    _inherit = 'hr.department'
+    _inherit = "hr.department"
 
     department_channel_ids = fields.One2many(
-        'mail.channel', readonly=True, string='Channels',
-        inverse_name='department_id',
+        "mail.channel",
+        readonly=True,
+        string="Channels",
+        inverse_name="department_id",
     )
 
     department_channel_count = fields.Integer(
-        compute="_compute_department_channel_count", store=True,
-        track_visibility='onchange',
+        compute="_compute_department_channel_count",
+        store=True,
+        track_visibility="onchange",
     )
 
-    @api.depends('department_channel_ids')
+    @api.depends("department_channel_ids")
     def _compute_department_channel_count(self):
         for record in self:
             record.department_channel_count = len(
@@ -28,27 +31,28 @@ class HrDepartment(models.Model):
         self.ensure_one()
         if not employee.user_id:
             return
-        self.department_channel_ids.write({
-            'channel_partner_ids': [(4, employee.user_id.partner_id.id)]
-        })
+        self.department_channel_ids.write(
+            {"channel_partner_ids": [(4, employee.user_id.partner_id.id)]}
+        )
 
     def _remove_employee_channel(self, employee):
         if not employee.user_id or (
-            employee == self.manager_id and employee.department_id.id == self.id
+            employee == self.manager_id
+            and employee.department_id.id == self.id
         ):
             return
-        self.department_channel_ids.write({
-            'channel_partner_ids': [(3, employee.user_id.partner_id.id)]
-        })
+        self.department_channel_ids.write(
+            {"channel_partner_ids": [(3, employee.user_id.partner_id.id)]}
+        )
 
     @api.multi
     def write(self, vals):
-        if 'manager_id' in vals:
+        if "manager_id" in vals:
             for record in self:
                 if record.manager_id:
                     record._remove_employee_channel(record.manager_id)
         res = super().write(vals)
-        if 'manager_id' in vals:
+        if "manager_id" in vals:
             for record in self:
                 if record.manager_id:
                     record._add_employee_channel(record.manager_id)
@@ -57,14 +61,18 @@ class HrDepartment(models.Model):
     @api.multi
     def generate_channel(self):
         self.ensure_one()
-        members = self.member_ids.mapped('user_id')
+        members = self.member_ids.mapped("user_id")
         if self.manager_id.user_id or False:
             members |= self.manager_id.user_id
-        partners = members.mapped('partner_id')
-        self.department_channel_ids = [(0, 0, {
-            'name': self.name,
-            'public': 'private',
-            'channel_partner_ids': [
-                (6, 0, partners.ids)
-            ]
-        })]
+        partners = members.mapped("partner_id")
+        self.department_channel_ids = [
+            (
+                0,
+                0,
+                {
+                    "name": self.name,
+                    "public": "private",
+                    "channel_partner_ids": [(6, 0, partners.ids)],
+                },
+            )
+        ]
