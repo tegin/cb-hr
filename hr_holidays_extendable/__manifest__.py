e88448402f995ee862a8ba167f9fd06459c15261
Alba Riera <alba.riera@creublanca.es>
2021-07-13 12:44:23 +0200

    [MIG] hr_holidays_extendable: Migration to 13.0

--- hr_holidays_extendable/__manifest__.py
+++ hr_holidays_extendable/__manifest__.py
@@ -5,11 +5,11 @@
     "name": "Hr Holidays Extendable",
     "summary": """
         Allows a certain type of holidays to be extended.""",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/hr",
-    "depends": ["cb_holidays_compute_mix"],
+    "depends": ["hr_holidays_public"],
     "data": [
         "wizards/wizard_extend_holidays.xml",
         "views/hr_leave_views.xml",
--- hr_holidays_extendable/models/hr_leave.py
+++ hr_holidays_extendable/models/hr_leave.py
@@ -11,3 +11,18 @@ class HrLeave(models.Model):
     extendable = fields.Boolean(
         related="holiday_status_id.extendable", store=True, readonly=True
     )
+
+    def _check_date_state(self):
+        if not self.env.context.get("no_check_state_date"):
+            super(HrLeave, self)._check_date_state()
+
+    def _get_number_of_days(self, date_from, date_to, employee_id):
+        """ Returns a float equals to the timedelta between two dates given as string.
+        We need to modify in order to add the compute_leaves = False
+        """
+        if not self.env.context.get("no_check_state_date") or not employee_id:
+            return super()._get_number_of_days(date_from, date_to, employee_id)
+        employee = self.env["hr.employee"].browse(employee_id)
+        return employee._get_work_days_data_batch(
+            date_from, date_to, compute_leaves=False
+        )[employee.id]
