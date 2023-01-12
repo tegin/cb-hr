0cd0d5423848ee83501be16093acfd9e45ca315d
Enric Tobella <etobella@creublanca.es>
2023-01-12 14:03:47 +0100

    [MIG] cb_number_of_holidays_report: Migration to 14.0

--- cb_number_of_holidays_report/__manifest__.py
+++ cb_number_of_holidays_report/__manifest__.py
@@ -5,12 +5,13 @@
     "name": "Cb Number Of Holidays Report",
     "summary": """
         Report para saber quien tiene vacaciones en un intervalo de tiempo""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Creu Blanca",
+    "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-hr",
     "depends": ["cb_hr_views"],
     "data": [
+        "security/ir.model.access.csv",
         "views/hr_leave_type.xml",
         "report/holidays_count_report.xml",
         "wizards/wizard_holidays_count.xml",
