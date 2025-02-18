461f28532415b4342c83b117a3e32d970867197f
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-02-18 13:33:26 +0100

    [MIG] hr_holidays_extendable: Migration to 16.0

--- hr_holidays_extendable/__manifest__.py
+++ hr_holidays_extendable/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Hr Holidays Extendable",
     "summary": """
         Allows a certain type of holidays to be extended.""",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "CreuBlanca",
     "website": "https://github.com/tegin/cb-hr",
--- hr_holidays_extendable/tests/test_hr_holidays_extendable.py
+++ hr_holidays_extendable/tests/test_hr_holidays_extendable.py
@@ -19,14 +19,13 @@ class TestHrHolidaysExtendable(TransactionCase):
             {
                 "name": "Leave Type Test",
                 "extendable": True,
-                "allocation_type": "fixed",
-                "validity_start": False,
             }
         )
         self.allocation = self.env["hr.leave.allocation"].create(
             {
                 "name": "Test",
                 "employee_id": self.employee.id,
+                "date_from": date(2019, 1, 1),
                 "holiday_status_id": self.holiday_type.id,
                 "number_of_days": 10,
             }
@@ -35,18 +34,19 @@ class TestHrHolidaysExtendable(TransactionCase):
 
     def test_hr_holidays_extendable(self):
 
-        holiday = Form(self.env["hr.leave"])
-        holiday.name = "Test"
-        holiday.employee_id = self.employee
+        holiday = Form(
+            self.env["hr.leave"].with_context(default_employee_id=self.employee.id)
+        )
+        holiday.holiday_type = "employee"
         holiday.holiday_status_id = self.holiday_type
         holiday.request_date_from = date(2019, 5, 21)
         holiday.request_date_to = date(2019, 5, 22)
         holiday = holiday.save()
         holiday.action_validate()
 
-        remaining_days = self.holiday_type.get_days(self.employee.id)[
-            self.holiday_type.id
-        ]["remaining_leaves"]
+        remaining_days = self.holiday_type.get_employees_days([self.employee.id])[
+            self.employee.id
+        ][self.holiday_type.id]["remaining_leaves"]
         self.assertEqual(remaining_days, 8)
 
         wizard = (
@@ -61,7 +61,7 @@ class TestHrHolidaysExtendable(TransactionCase):
 
         self.assertEqual(holiday.request_date_to, date(2019, 5, 23))
         self.assertEqual(holiday.number_of_days, 3)
-        remaining_days = self.holiday_type.get_days(self.employee.id)[
-            self.holiday_type.id
-        ]["remaining_leaves"]
+        remaining_days = self.holiday_type.get_employees_days([self.employee.id])[
+            self.employee.id
+        ][self.holiday_type.id]["remaining_leaves"]
         self.assertEqual(remaining_days, 7)
